15bb6641756caf19afe26b97c1d07df5163094ff
aleksandr <vaytehovich.aleksandr@mail.ru>
2019-10-31 20:30:09 +0300

    update actor

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,7 +4,7 @@ export default function Hero({actors,removeActors}) {
 
     return (
         <div className="d-flex justify-content-between widthContent">
-            <h2>
+            <h2 className="border border-secondary rounded pl-3 pr-3 bg-light">
             {actors.name}
             </h2>
             <button type="submit" className="m-1 btn btn-danger" onClick={() => removeActors(actors.name)}>X</button>
