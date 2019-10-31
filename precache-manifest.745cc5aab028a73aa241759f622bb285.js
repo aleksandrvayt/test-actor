bc430feb104cac514a07a87f96590f94c47dd96e
aleksandr <vaytehovich.aleksandr@mail.ru>
2019-10-31 13:20:40 +0300

    Updates

---
+++ precache-manifest.745cc5aab028a73aa241759f622bb285.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb7e0acb2f28c2a30b7061e297679ec1",
+    "url": "/test-actor/index.html"
+  },
+  {
+    "revision": "721da220a067293d221a",
+    "url": "/test-actor/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "872755394547b2f7f9ba",
+    "url": "/test-actor/static/css/main.61024516.chunk.css"
+  },
+  {
+    "revision": "721da220a067293d221a",
+    "url": "/test-actor/static/js/2.f95e5e08.chunk.js"
+  },
+  {
+    "revision": "872755394547b2f7f9ba",
+    "url": "/test-actor/static/js/main.482f3b4f.chunk.js"
+  },
+  {
+    "revision": "58e219ad8264785205dd",
+    "url": "/test-actor/static/js/runtime-main.7e1dc052.js"
+  },
+  {
+    "revision": "62b8fa7b4b55e26abdd4aa398ca9763a",
+    "url": "/test-actor/static/media/actors.62b8fa7b.jpg"
+  }
+]);
